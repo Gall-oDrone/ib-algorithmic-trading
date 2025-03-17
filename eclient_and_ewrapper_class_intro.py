df605235c1b5ffd29fdf710639db37c7e1e1c9e9
Gall-oDrone <piehavok@hotmail.com>
2025-03-17 13:03:31 -0600

    Getting Historical Data  Using  IBAPI

--- eclient_and_ewrapper_class_intro.py
+++ eclient_and_ewrapper_class_intro.py
@@ -1,3 +1,9 @@
+"""
+IBAPI - Getting historical data intro
+
+@author: Diego Gallo
+"""
+
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
@@ -17,12 +23,15 @@ class TradingApp(EWrapper, EClient):
     def contractDetails(self, reqId, contractDetails):
         print(f"reqID:{reqId}, contract:{contractDetails}")
 
+    def historicalData(self, reqId, bar):
+        print("HistoricalData. ReqId:", reqId, "BarData.", bar)
+
 def websocket_conn():
     app.run()
     event.wait()
     if event.is_set():
         app.close()
-        
+
 event = threading.Event()
 app = TradingApp()
 app.connect("127.0.0.1", 4002, clientId=0)
@@ -35,8 +44,20 @@ contract = Contract()
 contract.symbol = "NVDA"
 contract.secType = "STK"
 contract.currency = "USD"
-contract.exchange = "ISLAND"
+contract.exchange = "NASDAQ"
 
-app.reqContractDetails(100, contract)
+#app.reqContractDetails(100, contract)
+app.reqHistoricalData(
+    reqId=1,
+    contract=contract,
+    endDateTime='',
+    durationStr="1 D",
+    barSizeSetting="5 mins",
+    whatToShow="BID_ASK",
+    useRTH=1,
+    formatDate=1,
+    keepUpToDate=False,
+    chartOptions=[]
+)
 time.sleep(5)
 event.set()
