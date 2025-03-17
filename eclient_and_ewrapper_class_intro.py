3f4513a297b3741af6511c27b29bd4956829f442
Gall-oDrone <piehavok@hotmail.com>
2025-03-16 19:58:26 -0600

    Getting Contract Info

--- eclient_and_ewrapper_class_intro.py
+++ eclient_and_ewrapper_class_intro.py
@@ -1,13 +1,28 @@
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
 
 class TradingApp(EWrapper, EClient):
     def __init__(self, *args, **kwargs):
         EClient.__init__(self, self)
         
     def error(self, reqId, errorCode, errorString, advancedOrderRejectJson):
-        print("Error {} {} {}".format(reqId,errorCode,errorString))
+        if advancedOrderRejectJson:
+            print("Error {} {} {} {}".format(reqId,errorCode,errorString,advancedOrderRejectJson))
+        else:
+            print("Error {} {} {}".format(reqId,errorCode,errorString))
+
+    def contractDetails(self, reqId, contractDetails):
+        print(f"reqID:{reqId}, contract:{contractDetails}")
 
 app = TradingApp()
 app.connect("127.0.0.1", 4002, clientId=0)
+
+contract = Contract()
+contract.symbol = "NVDA"
+contract.secType = "STK"
+contract.currency = "USD"
+contract.exchange = "SMART"
+
+app.reqContractDetails(100, contract)
 app.run()
